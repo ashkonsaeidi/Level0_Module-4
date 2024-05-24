1602217af45ad18926673892e48a22498bd21fda
league <57720048+richardson-liou@users.noreply.github.com>
2024-05-24 16:59:29 -0700

    I created while loops

--- _01_and_or/_b_test_scores.py
+++ _01_and_or/_b_test_scores.py
@@ -13,16 +13,30 @@ from tkinter import messagebox, simpledialog, Tk
 #   D = 59.5 to less than 69.5
 #   F = less than 59.5
 if __name__ == '__main__':
+    w = Tk()
+    w.withdraw()
     # TODO) Ask the user for their score on the FIRST test and store their
     #  score in a variable
-
+    score = simpledialog.askinteger(None, prompt="What was your first test score?")
     # TODO) Ask the user for their score on the SECOND test and store their
     #  score in a variable
-
+    score2 = simpledialog.askinteger(None, prompt="What was your second test score?")
     # TODO) Take the average score of both tests (total score / 2)
-
+    average = (score2 + score)/2
+    messagebox.showinfo(None, average)
+    print(average)
     # TODO) Use if statements to check the average score and print the
     #  corresponding letter grade back to the user. Also, give a different
     #  message according to their grade. Example, for an 'A' grade:
     #  "Wow! You must have studied really hard for those tests!"
+    if average > 89.5:
+        messagebox.showinfo(None, "Wow! You must have studied hard for those tests!")
+    if average > 79.5 and average < 89.5:
+        messagebox.showinfo(None, "You got a B, good job!")
+    if average > 69.5 and average < 79.5:
+        messagebox.showinfo(None, "Its average, but still savage!")
+    if average > 59.5 and average < 69.5:
+        messagebox.showinfo(None, "Not a good grade, maybe try harder next time!")
+    if average < 59.5:
+        messagebox.showinfo(None, "Dude, your so bad, please kys")
     pass
--- _03_while_loops/_b_pi_aloud.py
+++ _03_while_loops/_b_pi_aloud.py
@@ -10,21 +10,29 @@ if __name__ == '__main__':
     window.withdraw()
     # TODO) Place the first 20 digits of pi into a variable as a string
     #  3.1415926535897932384
-    pi = '3.1415926535897932384'
+
     # TODO) Print out the first 3 digits of pi. For example,
     #  pi_str[0]   # first digit
     #  pi_str[1]   # second digit
-    pi_str = '3.1415926535897932384'
+    pi_str = '31415926535897932384'
     print(pi_str[0])
     print(pi_str[2])
     print(pi_str[3])
 
     # TODO) Use a while loop to keep asking for the next digit of pi
+    answer = simpledialog.askinteger(None, prompt = "What is the next letter of pi?")
+    score = 3
+    while score < len(pi_str):
+        answer
+    if answer == pi_str[score]:
+        score += 1
+    else:
+        exit()
+
 
         # TODO) If they are correct, print "correct".
         #  If they are not, print "incorrect" and break out of the while loop
-    answer = simpledialog.askstring(None, prompt="What the next digit of pi after 3.14")
-    for i in range(len(pi)):
+
 
 
     # TODO) Print out how many digits of pi the user was able to recite
